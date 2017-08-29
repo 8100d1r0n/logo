6deaf1267e3a9471af48327c863a5a71de502b0f
8100d1r0n <8100d1r0n@riseup.net>
2017-08-29 15:50:14 +0300

    UP

--- freebsdinfo.c
+++ freebsdinfo.c
@@ -2,38 +2,38 @@
  * -- FreeBSD Info Program --
  *    April. 2015.
 */
-#include <sys/utsname.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <pwd.h>
+#include <sys/utsname.h>
 #include <sys/time.h>
 #include <sys/sysctl.h>
 #include <sys/statvfs.h>
 
-#define Z0 "\x1b[0m"
-#define Z1 "\x1b[1;34m"
-#define Z2 "\x1b[1;37m"
-#define Z3 "\x1b[30m"
-#define Z4 "\x1b[31m"
-#define Z5 "\x1b[37m"
+#define RST "\x1b[0m"
+#define BLU "\x1b[1;34m"
+#define WHT "\x1b[1;37m"
+#define BLC "\x1b[30m"
+#define RED "\x1b[31m"
+#define GRY "\x1b[37m"
 #define BK "\e[1m\e[37;1;41m"
 #define BS "\e[1m\e[37;1;40m"
 #define KB "\e[1m\e[31;1;60m"
 #define MB "\e[1m\e[44;1;37m"
 
-static void disk(void);
-
 static void disk(void) {
     struct statvfs info;
+
     if(!statvfs("/", &info)) {
         unsigned long left  = (info.f_bfree * info.f_frsize);
         unsigned long total = (info.f_blocks * info.f_frsize);
         unsigned long used  = total - left;
         float perc  = (float)used / (float)total;
-        printf(Z5"%25s     %.2f%% of %.2f GB\n",Z4" Disk "Z5,
+        printf(GRY"%25s     %.2f%% of %.2f GB\n",RED" Disk "GRY,
                 perc * 100, (float)total / 1e+09);
- }
+     }
 }
  static void uptime(time_t *nowp) {
         struct timeval boottime;
@@ -42,34 +42,41 @@ static void disk(void) {
         int mib[2];
         size_t size;
         char buf[256];
-        if(strftime(buf, sizeof(buf), NULL, localtime(nowp)))
 
-        mib[0] = CTL_KERN;
-        mib[1] = KERN_BOOTTIME;
-        size = sizeof(boottime);
+        if(strftime(buf, sizeof(buf), NULL, localtime(nowp)))
+        	mib[0] = CTL_KERN;
+        	mib[1] = KERN_BOOTTIME;
+        	size = sizeof(boottime);
+
         if(sysctl(mib, 2, &boottime, &size, NULL, 0) != -1 &&
                 boottime.tv_sec) {
                 uptime = *nowp - boottime.tv_sec;
-                if(uptime > 60)
+
+        if(uptime > 60)
                 uptime += 30;
-                days = (int)uptime / 86400;
-                uptime %= 86400;
-                hrs = (int)uptime / 3600;
-                uptime %= 3600;
-                mins = (int)uptime / 60;
-                secs = uptime % 60;
-                printf(Z4"       Uptime"Z0);
-                if(days > 0)
-                        printf("%d day%s", days, days > 1? "s " : " ");
-                if (hrs > 0 && mins > 0)
-                        printf(Z5"    %02d:%02d"Z0, hrs, mins);
-                else if(hrs == 0 && mins > 0)
-                        printf(Z5"    0:%02d"Z0, mins);
-                else
-                        printf(Z5"    0:00"Z0);
-                putchar('\n');
-        }
-}
+       	        days = (int)uptime / 86400;
+               	uptime %= 86400;
+               	hrs = (int)uptime / 3600;
+               	uptime %= 3600;
+               	mins = (int)uptime / 60;
+               	secs = uptime % 60;
+               	printf(RED"       Uptime"RST);
+
+        if(days > 0)
+                printf("%d day%s", days, days > 1? "s " : " ");
+
+        if (hrs > 0 && mins > 0)
+                printf(GRY"    %02d:%02d"RST, hrs, mins);
+
+        else if(hrs == 0 && mins > 0)
+                printf(GRY"    0:%02d"RST, mins);
+ 
+               else
+		    printf(GRY"    0:00"RST);
+		    putchar('\n');
+        	}
+	}
+
 int main() {
 
 	FILE *fpt;
@@ -79,11 +86,14 @@ int main() {
 	pclose(fpt);
 
 	struct passwd *p;
-	uid_t uid=1001;
+	uid_t uid=1001; /* 1001 User id */
 
 	if ((p = getpwuid(uid)) == NULL)
 		perror("getpwuid() error");
 
+time_t now;
+time(&now);
+{
         char computer[256];
         struct utsname uts;
         time_t timeval;
@@ -94,29 +104,30 @@ int main() {
                 fprintf(stderr, "Could not get host information, so fuck off\n");
                 exit(1);
          }
-time_t now;
-time(&now);
-printf(Z4"%s\n","             ▄"Z0);
-printf(Z4"%s\n","          ▄███▀            ▀█▄"Z0);
-printf(Z4"%s\n","         ▓███               ▄█▒"Z0);
-printf(Z4"%s%s\n","        ▒▓██    ▓▄▄░▒▓▓░"Z0,Z4"▄▄▓░▓█▓"Z0);
-printf(Z4"%s","       ▐▒▓██▌ ▄████████████▓▒▀"Z0); uptime(&now);
-printf(Z4"%s%19s%15s\n","        ▀▒███▓████████████████"Z0,Z4"OS"Z5, uts.sysname);
-printf(Z4"%s%s%s%s%s%20s%10s\n","         ▀▒█████████"Z0,BK"▄▄"Z0,Z4"███"Z0,BK"▄██▄"Z0,Z4"█▄"Z0,Z4"User"Z5,p->pw_name);// getlogin());
-printf(Z4"%s%s%s%s%s%s%s%24s%18s\n","          ▀▓███████"Z0,Z2"████"Z0,Z4"██"Z0,BK"▐██"Z0,BS"▀"Z0,BK"▌"Z0,Z4"█"Z0,Z4"Hostname"Z5, computer);
-printf(Z4"%s%s%s%s%s%s%s%22s%6s\n","            ▀▒████"Z0,BK"▐██"Z0,BS"▀▀"Z0,Z4"███"Z0,BK"▐"Z0,BS" ▀█"Z0,Z4"█▌"Z0,Z4"Version"Z5, uts.release);
-printf(Z4"%s%s%s%s%s%22s%7s\n","             ██████"Z0,Z2"█"Z0,Z3"  "Z0,BS"▀▌"Z0,Z4"▀▄▄▄▄▀█▄▄"Z0,Z4"Hardware"Z5, uts.machine);
-printf(Z4"%s%s%s%18s%23s\n","            ▐▒█████▓"Z0,BK"▀▀"Z0,Z4"█████▓▀▄████"Z0,Z4"Shell"Z5, p->pw_shell);
-printf(Z4"%s%21s%13s\n","             ▒▓██▀ ████▄▀▀▄▄█▀██▀"Z0,Z4"Userdir"Z5, p->pw_dir);
-printf(Z4"%s%24s%6s","              ▀▓█▓▄ ▀▀▀▀▀▀▀▄▄█▀"Z0,Z4"Packages"Z5, packages);
-printf(Z4"%s","                 ▀▀███████▀"Z0); disk();
-printf(Z4"%s%23s%31s","               ▄▄▄███████▄▄▄"Z0,Z4"Date"Z5, ctime(&timeval));
-printf(Z4"%s\n","          ▄▄▄███▀▄▓█████▓▄▀███▄▄▄"Z0);
-printf(Z1"%s%s%s%s%s\n"," ▒▓██▓▓▓░"Z0,Z4"█████▄"Z0,Z1" ░▒▓████████"Z0,Z4"▄██████"Z0,Z1"░▒███████▓▓▓▒▄   ▄█████▓▒ ███████▓▄"Z0);
-printf(Z1"%s%s%s%s%s\n"," ▓███   "Z0,Z4"▓████ ▀██"Z0,Z1"  ░▒▓██▀"Z0,Z4" ██▀ █████ "Z0,Z1"   ██    ▀███ ▒██▀      ██    ▀█▓▒"Z0);
-printf(Z1"%s%s%s%s%s\n"," ████    "Z0,Z4"▀▓███"Z0,Z1"               "Z0,Z4"▐██▒▀"Z0,Z1"     ██    ▄██▀ ▒██▄      ██      ██▓"Z0);
-printf(Z1"%s\n"," ███████ ▓████▄█▓▒▄ ▄██▓▓▒▄   ▄██▓▓▒▄  ████████   ▀▓█████▄  ██      ██▓"Z0);
-printf(Z1"%s\n"," ████    ▓████  ▀▀▀░██▀  ▀██ ▒██▀  ▀██ ██    ▀██▄       ▀██ ██      ███"Z0);
-printf(Z1"%s\n"," ████    ▓████     ▒██▀▀▀▀▀▀ ███▀▀▀▀▀▀ ██    ▄███       ▄██ ██    ▄███"Z0);
-printf(Z1"%s\n"," ████    ▓████     ▀███▄▄██▀  ███▄▄██▀ ████████▀  ▒██████▀  ████████▀"Z0);
+
+	printf(RED"%s\n","             ▄"RST);
+	printf(RED"%s\n","          ▄███▀            ▀█▄"RST);
+	printf(RED"%s\n","         ▓███               ▄█▒"RST);
+	printf(RED"%s%s\n","        ▒▓██    ▓▄▄░▒▓▓░"RST,RED"▄▄▓░▓█▓"RST);
+	printf(RED"%s","       ▐▒▓██▌ ▄████████████▓▒▀"RST); uptime(&now);
+	printf(RED"%s%19s%15s\n","        ▀▒███▓████████████████"RST,RED"OS"GRY, uts.sysname);
+	printf(RED"%s%s%s%s%s%20s%10s\n","         ▀▒█████████"RST,BK"▄▄"RST,RED"███"RST,BK"▄██▄"RST,RED"█▄"RST,RED"User"GRY,p->pw_name);// getlogin());
+	printf(RED"%s%s%s%s%s%s%s%24s%18s\n","          ▀▓███████"RST,WHT"████"RST,RED"██"RST,BK"▐██"RST,BS"▀"RST,BK"▌"RST,RED"█"RST,RED"Hostname"GRY, computer);
+	printf(RED"%s%s%s%s%s%s%s%22s%6s\n","            ▀▒████"RST,BK"▐██"RST,BS"▀▀"RST,RED"███"RST,BK"▐"RST,BS" ▀█"RST,RED"█▌"RST,RED"Version"GRY, uts.release);
+	printf(RED"%s%s%s%s%s%22s%7s\n","             ██████"RST,WHT"█"RST,BLC"  "RST,BS"▀▌"RST,RED"▀▄▄▄▄▀█▄▄"RST,RED"Hardware"GRY, uts.machine);
+	printf(RED"%s%s%s%18s%23s\n","            ▐▒█████▓"RST,BK"▀▀"RST,RED"█████▓▀▄████"RST,RED"Shell"GRY, p->pw_shell);
+	printf(RED"%s%21s%13s\n","             ▒▓██▀ ████▄▀▀▄▄█▀██▀"RST,RED"Userdir"GRY, p->pw_dir);
+	printf(RED"%s%24s%6s","              ▀▓█▓▄ ▀▀▀▀▀▀▀▄▄█▀"RST,RED"Packages"GRY, packages);
+	printf(RED"%s","                 ▀▀███████▀"RST); disk();
+	printf(RED"%s%23s%31s","               ▄▄▄███████▄▄▄"RST,RED"Date"GRY, ctime(&timeval));
+	printf(RED"%s\n","          ▄▄▄███▀▄▓█████▓▄▀███▄▄▄"RST);
+	printf(BLU"%s%s%s%s%s\n"," ▒▓██▓▓▓░"RST,RED"█████▄"RST,BLU" ░▒▓████████"RST,RED"▄██████"RST,BLU"░▒███████▓▓▓▒▄   ▄█████▓▒ ███████▓▄"RST);
+	printf(BLU"%s%s%s%s%s\n"," ▓███   "RST,RED"▓████ ▀██"RST,BLU"  ░▒▓██▀"RST,RED" ██▀ █████ "RST,BLU"   ██    ▀███ ▒██▀      ██    ▀█▓▒"RST);
+	printf(BLU"%s%s%s%s%s\n"," ████    "RST,RED"▀▓███"RST,BLU"               "RST,RED"▐██▒▀"RST,BLU"     ██    ▄██▀ ▒██▄      ██      ██▓"RST);
+	printf(BLU"%s\n"," ███████ ▓████▄█▓▒▄ ▄██▓▓▒▄   ▄██▓▓▒▄  ████████   ▀▓█████▄  ██      ██▓"RST);
+	printf(BLU"%s\n"," ████    ▓████  ▀▀▀░██▀  ▀██ ▒██▀  ▀██ ██    ▀██▄       ▀██ ██      ███"RST);
+	printf(BLU"%s\n"," ████    ▓████     ▒██▀▀▀▀▀▀ ███▀▀▀▀▀▀ ██    ▄███       ▄██ ██    ▄███"RST);
+	printf(BLU"%s\n"," ████    ▓████     ▀███▄▄██▀  ███▄▄██▀ ████████▀  ▒██████▀  ████████▀"RST);
+      }
+   return 0;
 }
--- openbsdinfo.c
+++ openbsdinfo.c
@@ -2,39 +2,32 @@
  * -- OpenBSD Info Program --
  *      -- April.2015 --
 */
-#include <sys/utsname.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <pwd.h>
+#include <sys/utsname.h>
 #include <sys/time.h>
 #include <sys/sysctl.h>
 #include <sys/statvfs.h>
-#define Z0 "\x1b[0m"
-#define Z1 "\x1b[0;32m"
-#define Z2 "\x1b[1;33m"
-#define Z3 "\x1b[0;36m"
-#define Z4 "\x1b[0;31m"
-#define Z5 "\x1b[37m"
 
-static void disk(void);
-
-void help(void) {
-      printf(Z3" OpenBSD Info --- By:  8100d1r0n  April. 2015\n"
-             "-h help msg :)\n"Z0);
-      exit(0);
-}
+#define RST "\x1b[0m"
+#define YLW "\x1b[1;33m"
+#define RED "\x1b[0;31m"
+#define GRY "\x1b[37m"
 
 static void disk(void) {
     struct statvfs info;
+
     if(!statvfs("/", &info)) {
         unsigned long left  = (info.f_bfree * info.f_frsize);
         unsigned long total = (info.f_blocks * info.f_frsize);
         unsigned long used  = total - left;
         float perc  = (float)used / (float)total;
-        printf(Z5"%37s%s%18s%.2f%% of %.2f GB\n","║",Z4" Disk ",Z5"║ "Z5,
+        printf(GRY"%37s%s%18s%.2f%% of %.2f GB\n","║",RED" Disk ",GRY"║ "GRY,
                 perc * 100, (float)total / 1e+09);
- }
+    }
 }
  static void uptime(time_t *nowp) {
         struct timeval boottime;
@@ -43,57 +36,55 @@ static void disk(void) {
         int mib[2];
         size_t size;
         char buf[256];
+
         if(strftime(buf, sizeof(buf), NULL, localtime(nowp)))
-             mib[0] = CTL_KERN;
-        mib[1] = KERN_BOOTTIME;
-        size = sizeof(boottime);
-        if(sysctl(mib, 2, &boottime, &size, NULL, 0) != -1 &&
-                        boottime.tv_sec) {
-                uptime = *nowp - boottime.tv_sec;
-                if(uptime > 60)
-                        uptime += 30;
-                days = (int)uptime / 86400;
-                uptime %= 86400;
-                hrs = (int)uptime / 3600;
-                uptime %= 3600;
-                mins = (int)uptime / 60;
-                secs = uptime % 60;
-                printf(Z5"%37s%s%12s\b","║",Z4" Uptime",Z5"║ ");
-                if(days > 0)
-                        printf(" %d day%s", days, days > 1? "s " : " ");
-                if (hrs > 0 && mins > 0)
-                        printf(Z5" %02d:%02d"Z0, hrs, mins);
-                else if(hrs == 0 && mins > 0)
-                        printf(Z5" 0:%02d"Z0, mins);
-                else
-                        printf(Z5" 0:00"Z0);
-                putchar('\n');
-        }
-}
-int main(int argc, char **argv) {
+            mib[0] = CTL_KERN;
+            mib[1] = KERN_BOOTTIME;
+            size = sizeof(boottime);
+
+            if(sysctl(mib, 2, &boottime, &size, NULL, 0) != -1 &&
+                boottime.tv_sec) {
+	                uptime = *nowp - boottime.tv_sec;
+
+        		        if(uptime > 60)
+    		                uptime += 30;
+                			days = (int)uptime / 86400;
+                			uptime %= 86400;
+                			hrs = (int)uptime / 3600;
+                			uptime %= 3600;
+                			mins = (int)uptime / 60;
+                			secs = uptime % 60;
+                			printf(GRY"%37s%s%12s\b","║",RED" Uptime",GRY"║ ");
+
+                		if(days > 0)
+                        		printf(" %d day%s", days, days > 1? "s " : " ");
+
+                		if (hrs > 0 && mins > 0)
+                        		printf(GRY" %02d:%02d"RST, hrs, mins);
+
+                		else if(hrs == 0 && mins > 0)
+                        		printf(GRY" 0:%02d"RST, mins);
+
+                		else
+                        		printf(GRY" 0:00"RST);
+                			putchar('\n');
+        			}
+			}
+
+int main() {
 	FILE *fpt;
 	char packages[50] = " ";
-	fpt = popen ("pkg_info -q | gwc -l", "r");
+	//fpt = popen ("pkg_info -q | gwc -l", "r");
+	fpt = popen ("/bin/ls -h /var/db/pkg | /usr/local/bin/gwc -l", "r");
 	fgets(packages, 50, fpt);
 	pclose(fpt);
 
 	struct passwd *p;
-	uid_t uid=1000; // 1000 user uid number.
+	uid_t uid=1000; // 1000 user id number.
 
 	if ((p = getpwuid(uid)) == NULL)
 		perror("getpwuid() error");
 
-    if (argc >= 2) {
-        int c;
-        while ((c = getopt(argc, argv, "h")) != -1) {
-            switch (c) {
-                case 'h':
-                default:
-                    help();
-                    break;
-            }
-        }
-    }
 time_t now;
 time(&now);
 {
@@ -107,25 +98,26 @@ time(&now);
                 fprintf(stderr, "Could not get host information, so fuck off\n");
                 exit(1);
          }
-printf(Z5"▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n"Z0);
-printf(Z2"  ▄████▄  ▀██████████████████████████████████▄   ▄███████ ████████▄  \n"Z0);
-printf(Z2" ██    ██                            ██    ▀███ ███▀      ██    ▀███ \n"Z0);
-printf(Z2" ██    ██                            ██    ▄██▀ ███▄      ██      ███\n"Z0);
-printf(Z2" ██    ██ ██████▄   ▄████▄  ▄█████▄  ████████   ▀██████▄  ██      ███\n"Z0);
-printf(Z2" ██    ██ ██    ██ ██▀  ▀██ ██   ▀██ ██    ▀██▄       ▀██ ██      ███\n"Z0);
-printf(Z2" ██    ██ ██    ██ ██▀▀▀▀▀▀ ██    ██ ██    ▄███       ▄██ ██    ▄███ \n"Z0);
-printf(Z2"  ▀████▀ ▄██████▀  ▀██▄▄██▀ ██    ██ ████████▀  ███████▀  ████████▀  \n"Z0);
-printf(Z5"%s%s%s%s%s\n","▀▀▀▀▀▀▀▀▀▀",  Z2"██"Z0,Z5"▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"Z0,Z2"█▀"Z0,Z5"▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"Z0);
-printf(Z2"%20s%30s%s%16s%s\n",      " ██" Z0,Z5"║",Z4" OS",      Z5"║ ", uts.sysname);
-printf(Z2"%25s%29s%s%14s%s\n",     " ▀▀▀▀"Z0,Z5"║",Z4" User",    Z5"║ ", getlogin());
-printf(Z5"%37s%s%10s%s\n",                     "║",Z4" Hostname",Z5"║ ", computer);
-printf(Z5"%37s%s%11s%s\n",                     "║",Z4" Version", Z5"║ ", uts.release);
-printf(Z5"%37s%s%10s%s\n",                     "║",Z4" Hardware",Z5"║ ", uts.machine);
-printf(Z5"%37s%s%13s%s\n",                     "║",Z4" Shell",   Z5"║ ", p->pw_shell);
-printf(Z5"%37s%s%11s%s\n",                     "║",Z4" Userdir", Z5"║ ", p->pw_dir);
-printf(Z5"%37s%s%10s%s",                       "║",Z4" Packages",Z5"║ ", packages);disk();
-printf(Z5"%37s%s%14s%s",                       "║",Z4" Date",    Z5"║ ", ctime(&timeval));uptime(&now);
-printf(Z5"%74s\n",                             "╚══════════╝"Z0);
-  }
-}
 
+        printf(GRY"▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n"RST);
+        printf(YLW"  ▄████▄  ▀██████████████████████████████████▄   ▄███████ ████████▄  \n"RST);
+        printf(YLW" ██    ██                            ██    ▀███ ███▀      ██    ▀███ \n"RST);
+        printf(YLW" ██    ██                            ██    ▄██▀ ███▄      ██      ███\n"RST);
+        printf(YLW" ██    ██ ██████▄   ▄████▄  ▄█████▄  ████████   ▀██████▄  ██      ███\n"RST);
+        printf(YLW" ██    ██ ██    ██ ██▀  ▀██ ██   ▀██ ██    ▀██▄       ▀██ ██      ███\n"RST);
+        printf(YLW" ██    ██ ██    ██ ██▀▀▀▀▀▀ ██    ██ ██    ▄███       ▄██ ██    ▄███ \n"RST);
+        printf(YLW"  ▀████▀ ▄██████▀  ▀██▄▄██▀ ██    ██ ████████▀  ███████▀  ████████▀  \n"RST);
+        printf(GRY"%s%s%s%s%s\n","▀▀▀▀▀▀▀▀▀▀",  YLW"██"RST,GRY"▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"RST,YLW"█▀"RST,GRY"▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"RST);
+        printf(YLW"%20s%30s%s%16s%s\n",      " ██" RST,GRY"║",RED" OS",      GRY"║ ", uts.sysname);
+        printf(YLW"%25s%29s%s%14s%s\n",     " ▀▀▀▀"RST,GRY"║",RED" User",    GRY"║ ", getlogin());
+        printf(GRY"%37s%s%10s%s\n",                     "║",RED" Hostname",GRY"║ ", computer);
+        printf(GRY"%37s%s%11s%s\n",                     "║",RED" Version", GRY"║ ", uts.release);
+        printf(GRY"%37s%s%10s%s\n",                     "║",RED" Hardware",GRY"║ ", uts.machine);
+        printf(GRY"%37s%s%13s%s\n",                     "║",RED" Shell",   GRY"║ ", p->pw_shell);
+        printf(GRY"%37s%s%11s%s\n",                     "║",RED" Userdir", GRY"║ ", p->pw_dir);
+        printf(GRY"%37s%s%10s%s",                       "║",RED" Packages",GRY"║ ", packages);disk();
+        printf(GRY"%37s%s%14s%s",                       "║",RED" Date",    GRY"║ ", ctime(&timeval));uptime(&now);
+        printf(GRY"%74s\n",                             "╚══════════╝"RST);
+      }
+   return 0;
+}
